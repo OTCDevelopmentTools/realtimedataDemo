f80078d3dbc524e74c2a96f03470813b71889937
OTCDevelopmentTools <121923850+OTCDevelopmentTools@users.noreply.github.com>
2023-01-25 18:05:09 +0530

    Add files via upload
    Added DempAPI proj

---
+++ RealTimeDataDemoAPI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using RealTimeDataDemoAPI.Hubs;
+using RealTimeDataDemoAPI.Models;
+
+namespace RealTimeDataDemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public HomeController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        [Route("PushEmployee")]
+        public IActionResult PushEmployee(int Id, string name, string address)
+        {
+            Employee employee = new Employee();
+            employee.Id = Id;
+            employee.Name = name;
+            employee.Address = address;
+
+            _hubContext.Clients.All.SendAsync("ReceiveEmployee", employee);
+            return Ok("Done");
+        }
+
+        [HttpGet]
+        [Route("PushMessage")]
+        public IActionResult PushMessage(string message)
+        {
+            _hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+            return Ok("Done");
+        }
+    }
+}
---
+++ RealTimeDataDemoAPI/Hubs/NotificationHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealTimeDataDemoAPI.Models;
+
+namespace RealTimeDataDemoAPI.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public NotificationHub()
+        {
+
+        }
+
+        public void BroadcastEmployee(Employee Emp)
+        {
+            Clients.All.SendAsync("ReceiveEmployee", Emp);
+        }
+
+        public void BroadcastMessage(string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
